dfc5aca9091b4644cbca59cbacef3f94b587599e
Hedvig <hedvigf@kth.se>
2019-11-14 22:10:57 +0100

    Riddle function, on click function that show a canvas with a riddle on it, when you click present number 2. Improvments liketrigger instead of onclick, and an escape button will be implemented later. To escape, press escape key

---
+++ Christmas Hero/Assets/Scripts/RiddleOnClick.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class RiddleOnClick : MonoBehaviour
+{
+    public float rayLength;
+    public LayerMask layermask;
+    public Canvas canvas;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, rayLength, layermask))
+            {
+                Debug.Log(hit.collider.name);
+                canvas.GetComponent<Canvas>().enabled = true;
+
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            canvas.GetComponent<Canvas>().enabled = false;
+        }
+    }
+}
