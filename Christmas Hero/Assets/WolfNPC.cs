61814cf5f2b81754c37de2062a16defe3ca80b30
Akash <akashmenon@live.se>
2019-11-28 10:35:06 +0100

    Added Cow, Fish, Piranha, Sheep

---
+++ Christmas Hero/Assets/WolfNPC.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WolfNPC : MonoBehaviour
+{
+    private NavMeshAgent m_Agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Agent = gameObject.GetComponent<NavMeshAgent>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
