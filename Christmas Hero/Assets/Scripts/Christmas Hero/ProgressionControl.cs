3952ee50ed852dc25197668e09b5f4c627f2a1cf
Akash Menon <akashmenon@live.se>
2019-12-05 13:13:38 +0100

    Visual xmas progression #31
    Call on IncrementProgress() from ProgressionController when wanting to progress the level of decorations. (After delivery of a present for instance)
    - Use "NumpadPlus" to test it for now

---
+++ Christmas Hero/Assets/Scripts/Christmas Hero/ProgressionControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressionControl : MonoBehaviour
+{
+    public int progIndex = 1;
+    [SerializeField]
+    private GameObject christmasDeco;
+    private const int LAST_LVL = 4; //we start on 1
+    /// <summary>
+    /// Makes changes in the scene that adds to the progression towards Christmas. 
+    /// </summary>
+
+    void Start()
+    { 
+        if (gameObject.CompareTag("ChristmasDeco")) //If we put this script on ChristmasDeco
+        {
+            christmasDeco = gameObject;
+        }
+        else //If we just keep it as a script, not attached to anything
+        {
+            christmasDeco = GameObject.FindWithTag("ChristmasDeco");
+            if (christmasDeco == null) Debug.Log("Can't find ChristmasDeco gamebject");
+        }
+    }
+
+    void Update() //Just for testing purposes, wont need update()
+    {
+        if (Input.GetKeyUp(KeyCode.KeypadPlus))
+        {
+            IncrementProgress();
+        }
+    }
+
+    public void IncrementProgress()
+    {
+        if (progIndex > LAST_LVL) { Debug.Log("Max Xmas progression level reached already"); return; }
+        progIndex++;
+        transform.Find("Level " + progIndex).gameObject.SetActive(true);
+    }
+
+
+
+
+}
