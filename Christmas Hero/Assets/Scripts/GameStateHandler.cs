645d5401bd2ecadd6aea117e5dc16a99b81b2cc6
Akash Menon <akashmenon@live.se>
2019-12-08 12:21:23 +0100

    Snowfall with present delivery + light fix
    - Each present delivery now makes more snow fall
    - Some fixes to the light progression

--- Christmas Hero/Assets/Scripts/GameStateHandler.cs
+++ Christmas Hero/Assets/Scripts/GameStateHandler.cs
@@ -6,7 +6,14 @@ public class GameStateHandler : MonoBehaviour
 {
 
     private GameObject decorations;
+
     private Light dLight; //directional light over entire scene
+    private float lightIntensity = 0.25f;
+    private ParticleSystem snowSystem;
+    [SerializeField]
+    private float rateOverTime = 5f;
+    [SerializeField]
+    private float rateOverDist = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,17 +23,25 @@ public class GameStateHandler : MonoBehaviour
             child.gameObject.SetActive(false);
 
         dLight = GameObject.Find("Directional Light").GetComponent<Light>();
-
+        snowSystem = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<ParticleSystem>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var em = snowSystem.emission;
+        em.rateOverTime = rateOverTime;
+        em.rateOverDistance = rateOverDist;
+        dLight.intensity = lightIntensity;
     }
 
 
     public void incrementDecor() {
+        //Light and SnowParticle System incrementation
+        lightIntensity += 0.5f;
+        rateOverTime += 10f;
+        rateOverDist += 0.5f;
+
         Transform previousChild = null;
         foreach (Transform child in decorations.transform) {
             if(child.gameObject.activeSelf && previousChild != null && !previousChild.gameObject.activeSelf) {
@@ -38,6 +53,12 @@ public class GameStateHandler : MonoBehaviour
         if(previousChild != null)
             previousChild.gameObject.SetActive(true);
         //decorations.SetActive(true);
-        dLight.intensity += 0.5f;
+
+
+    }
+
+    void OnGUI()
+    {
+        rateOverTime = GUI.HorizontalSlider(new Rect(25, 45, 100, 30), rateOverTime, 5.0f, 200.0f);
     }
 }
